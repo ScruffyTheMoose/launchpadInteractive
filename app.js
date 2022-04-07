ebdce6276bf73583f94b9880a14c9840142d4003
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-04-07 16:17:25 -0500

    add key listeners

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ function failure() {
     console.log('Could not connect MIDI');
 }
 
-function succes(midiAccess) {
-    console.log(midiAccess);
+function success(midiAccess) {
+    midiAccess.addEventListener('statechange', updateDevices);
+    const inputs = midiAccess.inputs;
+
+    inputs.forEach((input) => {
+        input.addEventListener('midimessage', handleInput);
+    });
+}
+
+function updateDevices(event) {
+    console.log(event);
+}
+
+function handleInput(input) {
+    const command = input.data[0];
+    const keyID = input.data[1];
+    const type = input.data[2];
+
+    console.log(`command: ${command}, keyID: ${keyID}, type: ${type}`);
 }
